973a443aaad0cdca0b527c436a4253330e58f634
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2025-12-31 14:34:39 +0530

    feat: refactor App component to include user data fetching and message handling; update main entry point for Redux integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
-import { LogIn } from 'lucide-react'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Login from './pages/Login'
-import Layout from './pages/Layout.jsx'
 import Feed from './pages/Feed'
 import Messages from './pages/Messages'
 import ChatBox from './pages/ChatBox'
@@ -10,14 +8,66 @@ import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
-import { useUser } from '@clerk/clerk-react'
+import {useUser, useAuth} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
+import toast, {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchUser } from './features/user/userSlice'
+import { fetchConnections } from './features/connections/connectionsSlice'
+import { addMessage } from './features/messages/messagesSlice'
+import Notification from './components/Notification'
 
 const App = () => {
-  const { isSignedIn } = useUser();
+  const {user} = useUser()
+  const {getToken } = useAuth()
+  const {pathname} = useLocation()
+  const pathnameRef = useRef(pathname)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      if(user){
+      const token = await getToken()
+      dispatch(fetchUser(token))
+      dispatch(fetchConnections(token))
+      }
+    }
+    fetchData()
+    
+  },[user, getToken, dispatch])
+
+  useEffect(()=>{
+    pathnameRef.current = pathname
+  },[pathname])
+
+  useEffect(()=>{
+    if(user){
+      const eventSource = new EventSource(import.meta.env.VITE_BASEURL + '/api/message/' + user.id);
+
+      eventSource.onmessage = (event)=>{
+        const message = JSON.parse(event.data)
+
+        if(pathnameRef.current === ('/messages/' + message.from_user_id._id)){
+          dispatch(addMessage(message))
+        }else{
+          toast.custom((t)=>(
+            <Notification t={t} message={message}/>
+          ), {position: "bottom-right"})
+        }
+      }
+      return ()=>{
+        eventSource.close()
+      }
+    }
+  },[user, dispatch])
+  
   return (
     <>
+      <Toaster />
       <Routes>
-        <Route path='/' element={!isSignedIn ? <Login/> : <Layout/>}>
+        <Route path='/' element={ !user ? <Login /> : <Layout/>}>
           <Route index element={<Feed/>}/>
           <Route path='messages' element={<Messages/>}/>
           <Route path='messages/:userId' element={<ChatBox/>}/>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,24 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Missing Publishable Key')
 }
-
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
+    <BrowserRouter>
+     <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </ClerkProvider>
+     </Provider>  
+    </BrowserRouter>    
+  </ClerkProvider>
 )
